8a7bf5bfa704c28e72eb42f1340845bad846123f
Robert <robertyan64913@gmail.com>
2021-05-23 09:47:51 -0400

    added shop

--- popup.js
+++ popup.js
@@ -25,6 +25,10 @@ function getMessage(message, sender, sendResponse) {
 }
 
 
+show_blocklist();
+hide_shop();
+hide_timer();
+
 //---------------------------------------------------------- blocklist code
 
 
@@ -230,3 +234,33 @@ document.getElementById("shop_btn").addEventListener("click", function() {
     hide_blocklist();
     hide_timer();
 });
+
+document.getElementById("item1func").addEventListener("click", item1);
+function item1() {
+	console.log("click1");
+}
+	
+document.getElementById("item2func").addEventListener("click", item2);
+function item2() {
+	console.log("click2");
+}
+
+document.getElementById("item3func").addEventListener("click", item3);
+function item3() {
+	console.log("click3");
+}
+
+document.getElementById("item4func").addEventListener("click", item4);
+function item4() {
+	console.log("click4");
+}
+	
+document.getElementById("item5func").addEventListener("click", item5);
+function item5() {
+	console.log("click5");
+}
+
+document.getElementById("item6func").addEventListener("click", item6);
+function item6() {
+	console.log("click6");
+}
